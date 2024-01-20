5b52e5d62c8548c3ddaa884d3074eb14ebbadabd
JOSEPH-DAYO <151461864+JOSEPH-DAYO@users.noreply.github.com>
2024-01-20 21:21:26 +0100

    Add files via upload
    Tnx

---
+++ src/MarvelData.js
@@ -0,0 +1,94 @@
+export const characters = [
+ 
+  {
+    name: "Albert Einstein",
+    alias: "Albert Einstein",
+    img1: "https://cdn.pixabay.com/photo/2023/09/30/17/30/ai-generated-8286112_1280.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Einstein was a hard-headed child. When he was born, seeing the shape of the back of his head, his mother and family believed it was a malformation, Young Einstein had a hard time speaking. He spoke very slowly and without much volume. It was difficult for him to construct whole sentences, until at the age of 9 he began to speak normally. A compass sparked his interest in science.When Einstein was 5 years old and sick in bed, his father gave him a compass and the little boy was fascinated that the needle always pointed to the same place. This made him understand that there were forces that drove things where there was apparently nothing.",
+    ],
+    team: "Genius",
+    description:
+      "Einstein was a hard-headed child. When he was born, seeing the shape of the back of his head, his mother and family believed it was a malformation, Young Einstein had a hard time speaking. He spoke very slowly and without much volume. It was difficult for him to construct whole sentences, until at the age of 9 he began to speak normally. A compass sparked his interest in science.When Einstein was 5 years old and sick in bed, his father gave him a compass and the little boy was fascinated that the needle always pointed to the same place. This made him understand that there were forces that drove things where there was apparently nothing.",
+  },
+  {
+    name: "Elon Musk",
+    alias: "Elon Musk",
+    abilities: ["Elon Musk (born June 28, 1971, Pretoria, South Africa) South African-born American entrepreneur who cofounded the electronic-payment firm PayPal and formed SpaceX, maker of launch vehicles and spacecraft. He was also one of the first significant investors in, as well as chief executive officer of, the electric car manufacturer Tesla. In addition, Musk acquired Twitter (later X) in 2022."],
+    team: "Tesla",
+    img1: "https://www.theglobeandmail.com/resizer/v2/TVMCFYJ5LFOC7CP5TR46LU5MJY.jpg?auth=69cd87806310ebc6f8676f7bb9e1cafd3f26a8029669938f5f1db321edb5ef51&width=1200&quality=80",
+    homeworld: "Earth",
+    description:
+      "Elon Musk (born June 28, 1971, Pretoria, South Africa) South African-born American entrepreneur who cofounded the electronic-payment firm PayPal and formed SpaceX, maker of launch vehicles and spacecraft. He was also one of the first significant investors in, as well as chief executive officer of, the electric car manufacturer Tesla. In addition, Musk acquired Twitter (later X) in 2022.",
+  },
+  {
+    name: "Aliko Dangote",
+    alias: "Aliko Dangote",
+    abilities: ["Dangote's fortune is primarily built from his company, Dangote Cement, although he started his business empire by selling commodities such as sugar, salt, and flour. He has also been the riches man in Africa for the past ten years."],
+    team: "Industrous",
+    img1: "https://image.cnbcfm.com/api/v1/image/105608495-1543947913488aliko.jpg?v=1543948096",
+    homeworld: "Earth",
+
+    description:
+      "Dangote's fortune is primarily built from his company, Dangote Cement, although he started his business empire by selling commodities such as sugar, salt, and flour. He has also been the riches man in Africa for the past ten years.",
+  },
+  {
+    name: "Don Moen",
+    alias: "Don Moen",
+    abilities: ["When you get to be my age, you reminisce about your life and wonder how many more years the Lord will give you to make a difference. My journey with music began when I was 8 years old the day the famous violinist, Dave Rubinoff, visited my 2nd grade classroom in the small Minnesota town where I grew up. Dave played “The Hot Canary” and made his violin do funny things. I went home that afternoon and told my mother and father I wanted to play the violin…all because this guy had made me laugh. We had no idea then how music would eventually become the central mission of my life."],
+    team: "Musical",
+    img1: "https://media.licdn.com/dms/image/C4D03AQGxbR1gLlFV2w/profile-displayphoto-shrink_800_800/0/1582549797180?e=2147483647&v=beta&t=rAPy0GexTpZmZaKZhg-hABT420pve0KXbBHFV3ndtwc",
+    homeworld: "Earth",
+
+    description:
+      "When you get to be my age, you reminisce about your life and wonder how many more years the Lord will give you to make a difference. My journey with music began when I was 8 years old the day the famous violinist, Dave Rubinoff, visited my 2nd grade classroom in the small Minnesota town where I grew up. Dave played “The Hot Canary” and made his violin do funny things. I went home that afternoon and told my mother and father I wanted to play the violin…all because this guy had made me laugh. We had no idea then how music would eventually become the central mission of my life.",
+  },
+
+ 
+  {
+    name: "Nelson Mandela",
+    alias: "Nelson Mandela",
+    abilities: ["Nelson Mandela was the president of South Africa from 1994-1999. He was the first black president of South Africa, and the first president to be elected in a fully representative election. Nelson Mandela's government focused on destroying the Apartheid government in the country, which had focused on racial segregation enforced by the law. In school, Mandela studied law and became one of South Africa’s first black lawyers."],
+    team: "Champions",
+    img1: "https://ocdn.eu/images/pulscms/ZmY7MDA_/18f77b622da63df7b778ff134f036a20.jpg",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Nelson Mandela was the president of South Africa from 1994-1999. He was the first black president of South Africa, and the first president to be elected in a fully representative election. Nelson Mandela's government focused on destroying the Apartheid government in the country, which had focused on racial segregation enforced by the law. In school, Mandela studied law and became one of South Africa’s first black lawyers.",
+  },
+
+  {
+    name: "Usain Bolt",
+    alias: "Usain Bolt",
+    abilities: ["Usain Bolt has been one of the biggest names for every Olympics in the past few years, including the Rio 2016 Olympics. And He did not disappoint with his memorable performance in the 100m sprint event. Here are some interesting facts about the worlds fastest man – Usain Bolt."],
+    team: "Agents of Atlas",
+    img1: "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_675,w_1200,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1492804042/articles/2012/08/04/how-usain-bolt-could-break-his-world-record-without-running-faster-in-the-olympics/track-hitting-speed-limit-barrow_vudty4",
+    homeworld: "Earth",
+    description:
+      "Usain Bolt has been one of the biggest names for every Olympics in the past few years, including the Rio 2016 Olympics. And He did not disappoint with his memorable performance in the 100m sprint event. Here are some interesting facts about the worlds fastest man – Usain Bolt.",
+  },
+
+  {
+    name: "Jayjay Okocha",
+    alias: "Jayjay Okocha",
+    abilities: ["Jay Jay Okocha was born in Enugu, Nigeria, on the 14th day of August 1973. He was only a youth player at his hometown in Enugu State for a while before crossing over to Europe to build his career. Other famous footballers that have played for Enugu Rangers includes Taribo West and John Utaka."],
+    team: "Nigeria Team",
+    img1: "https://usercontent.one/wp/www.busybuddiesng.com/wp-content/uploads/2020/01/IMG_20200131_183359.jpg",
+    homeworld: "Earth",
+    description:
+      "Jay Jay Okocha was born in Enugu, Nigeria, on the 14th day of August 1973. He was only a youth player at his hometown in Enugu State for a while before crossing over to Europe to build his career. Other famous footballers that have played for Enugu Rangers includes Taribo West and John Utaka.",
+  },
+  {
+    name: "Kenneth Hagin",
+    alias: "Kenneth Hagin",
+    abilities: ["On August 8, 1934, he says he was raised from his deathbed by a revelation of -faith in Gods Word- after reading Mark 11:23–24.[5] He was also healed of his paralysis and never struggled with walking. His dramatic healing is detailed in Roberts Liardons book Gods Generals."],
+    team: "Avengers",
+    img1: "https://upload.wikimedia.org/wikipedia/commons/9/92/Brother_E_Hagin.jpg",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
